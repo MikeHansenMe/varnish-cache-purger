83d609bb6381e84317604dcb534e1816de7a8976
Garth <jmort47@hotmail.com>
2014-09-08 14:34:25 -0600

    Admin bar mostly works now.

--- inc/admin-bar.php
+++ inc/admin-bar.php
@@ -5,8 +5,19 @@ add_action( 'admin_init', 'vp_adminbar_purge_all', 1 );
 function vp_adminbar_purge_all() {
 
 	if( ! current_user_can( 'manage_options' ) ) { return; }
+	$url = home_url();
+	if( is_ssl() == 'true' ) {
+		$page = 'https://' . preg_replace('#^.*://#', '', $url);
+	} else {
+		$page = 'http://' . preg_replace('#^.*://#', '', $url);
+	}
 
 	if( isset( $_GET['vp_submit'] ) && $_GET['vp_purge'] == 'all' ) {
+		$info = wp_remote_request( $page, array( 'method' => 'BAN', 'host' => home_url() ) );
+	
+
+/*
+		Here is the curl session for reference.
 		$page = get_option( 'siteurl' );
 		$res = curl_init( $page );
 		curl_setopt( $res, CURLOPT_RETURNTRANSFER, true );	
@@ -14,16 +25,22 @@ function vp_adminbar_purge_all() {
 		$content = curl_exec( $res );
 		$info = curl_getinfo( $res );
 		curl_close( $res );
+*/
 
-		if( $info['http_code'] == '200' ) {
-			add_action( 'admin_notices', 'vp_200' );
-			return;
-		} else if( $info['http_code'] == '404' ) {
-			add_action( 'admin_notices', 'vp_404' );
-			return;
-		} else if( $info['http_code'] == '405' ) {
-			add_action( 'admin_notices', 'vp_405' );
-			return;
+		if( ! empty( $info ) ) {
+			if( $info['response']['code'] == '200' ) {
+				add_action( 'admin_notices', 'vp_200' );
+				return;
+			} else if( $info['response']['code'] == '404' ) {
+				add_action( 'admin_notices', 'vp_404' );
+				return;
+			} else if( $info['response']['code'] == '405' ) {
+				add_action( 'admin_notices', 'vp_405' );
+				return;
+			} else {
+				add_action( 'admin_notices', 'vp_general_error' );
+				return;
+			}
 		} else {
 			add_action( 'admin_notices', 'vp_general_error' );
 			return;
@@ -36,11 +53,8 @@ add_action( 'admin_bar_menu', 'vp_adminbar', 200.5 );
 function vp_adminbar( $wp_admin_bar ) {
 	$args = array(
 		'id'	=> 'varnish_cache_purger',
-		'title' => '' . __( 'Varnish Cache Purger' ) . '<form name="varnish-purge-all" action="" method="GET">
-				<input type="hidden" name="vp_purge" value="all" />
-				<input type="submit" name="vp_submit" value="true" />
-				</form>',
-		'href'	=> '#',
+		'title' => 'Varnish Cache Purger',
+		'href'	=> '?vp_purge=all&vp_submit=true',
 	);
 	$wp_admin_bar->add_node( $args );
 }
