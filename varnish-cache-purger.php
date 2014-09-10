8a369a9556135a16dd4145beafeb90b1f425a51b
Garth <jmort47@hotmail.com>
2014-09-10 10:21:44 -0600

    Add plugin updater.

--- varnish-cache-purger.php
+++ varnish-cache-purger.php
@@ -3,10 +3,11 @@
 
 Plugin Name: Varnish Cache Purger
 Plugin URI: https://github.com/voldemortensen/varnish-cache-purger
-Description: Purges your varnish cache on certain changes and on demand.
+Description: Purges your varnish cache on demand.
 Version: 0.1
 Author: Garth Mortensen
-
+GitHub Plugin URI: https://github.com/voldemortensen/varnish-cache-purger
+GitHub Branch: master
 */
 
 if( ! defined( 'WPINC' ) ) { die; }
@@ -18,3 +19,29 @@ require_once( VP_BASE_DIR . 'inc/menu.php' );
 require_once( VP_BASE_DIR . 'inc/main.php' );
 require_once( VP_BASE_DIR . 'inc/admin-bar.php' );
 require_once( VP_BASE_DIR . 'inc/errors.php' );
+
+function vp_load_updater() {
+	if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+		$github = array(
+			'updater'	=> VP_BASE_DIR . 'updater/class-github-updater.php',
+			'api'		=> VP_BASE_DIR . 'updater/class-github-api.php',
+			'plugin'	=> VP_BASE_DIR . 'updater/class-plugin-updater.php'
+		);
+
+		if( ! class_exists( 'GitHub_Updater' ) && file_exists( $github['updater'] ) ) {
+			require_once( $github['updater'] );
+		}
+
+		if( ! class_exists( 'GitHub_Updater_GitHub_API' ) && file_exists( $github['api'] ) ) {
+			require_once( $github['api'] );
+		}
+
+		if( ! class_exists( 'GitHub_Plugin_Updater' ) && file_exists( $github['plugin'] ) ) {
+			require_once( $github['plugin'] );
+		}
+
+		new GitHub_Plugin_Updater;
+	}
+}
+add_action( 'admin_init', 'vp_load_updater' );
